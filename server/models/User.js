1695b4befb920cc89bdb5ec58036039f0fa00083
Phil Ershadi <phildoge@gmail.com>
2017-04-02 23:44:49 -0400

    Added 3 models, still at least 3 to go

--- server/models/User.js
+++ server/models/User.js
@@ -1,31 +1,61 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+var mongoose = require("mongoose");
 
-// ************************ User Schema *************************//
-
-const UserSchema = new Schema({
-
-  password: {
-    type: String,
-    required: true
-  },
-
-// do I need a confirmPass here?
-
-  username:{type:String, required:true},
-
-  role: {
-    type: String,
-    enum: ['Player','Admin'],
-    default: 'Player'
-  },
+var UserSchema = new mongoose.Schema({
+  username: {type String, required: true},
+  password: {type: String, required: true},
+  userRole: {type: String, enum:["player", "Admin"], default: "player"},
   resetPasswordToken: { type: String },
-  resetPasswordExpires: { Type: Date }
+  resetPasswordExpires: { Type: Date },
+
+  statistics:{ //The numbers for skill checks
+    strength: {type: Number, default: 17},
+    dexterity: {type: Number, default: 24},
+    endurance: {type: Number, default: 21},
+    reflexes: {type: Number, default: 22},
+    intellect: {type: Number, default: 27},
+    senses: {type: Number, default: 30}
+  },
+
+  status:{
+    currentHealth: {type: Number, default: 105}, //your health currently
+    currentMaxHealth: {type: Number, default: 105}, //if your max health is reduced through injury...
+    absoluteMaxHealth: {type: Number, default: 105}, // Your maximum health
+    currentSpell: {type: Number, default: 335},//your spell points currently
+    currentMaxSpell: {type: Number, default: 335}, //if max spell points are lowered due to active enchantments...
+    absoluteMaxSpell: {type: Number  default: 335}//your maximum spell points
+  },
+
+  enchantments: {//list of enchantments currently active
+    name: [String],
+    cost: [Number]  //TODO: Maybe make a schema for these as well?
+  },
+  known_spells: {type: [String], default: ["sparks"]}, //spells in spell list
+  inventory: [String], //items in inventory
+
+  position:{
+    plane: {type: String, default: "physical", enum:["physical","astral","limbo"]},
+    x: {type: Number, default: 0}, //x coordinate on map
+    y: {type: Number, default: -1}, //y coordinate on map
+  },
+
+  progress:{
+    checkpoint: {type: String, default: "a"},
+    time: {type: Number, default: 100},
+    nightTime: {type: Boolean, default: false},
+    flags: [FlagSchema]  //BIG TODO: Flag Schema how many flags do we need, how to make them?
+  }
 },
-{
-  timestamp: true
+{timestamp: true}
+);
+
+module.exports = mongoose.model("User", UserSchema);
+/*
+  to import, use this when you're going to create a profile:
+  var User = require(__directory + './models/User')
+
+  var newUser = new User ({
+    username: Jim Bob,
+    password: the_first_three_digits_of_your_dog
 })
 
- 
-  module.exports = mongoose.model('User', UserSchema)
-
+*/
